21cc65ef26947005a7847b770b02d7cead7a4e64
ramohin1236 <mohinr26@gmail.com>
2025-11-29 12:21:13 +0600

    chat provider added

---
+++ src/components/context/socketProvider.jsx
@@ -0,0 +1,88 @@
+// contexts/SocketContext.js
+"use client";
+
+import { createContext, useContext, useEffect, useRef, useState } from "react";
+import { io } from "socket.io-client";
+import { useSelector } from "react-redux";
+
+const SocketContext = createContext(null);
+
+export const useSocket = () => {
+  const context = useContext(SocketContext);
+  if (!context) {
+    throw new Error("useSocket must be used within a SocketProvider");
+  }
+  return context;
+};
+
+export const SocketProvider = ({ children }) => {
+  const socketRef = useRef(null);
+  const [socketLoading, setSocketLoading] = useState(false);
+  const [isConnected, setIsConnected] = useState(false);
+
+  const { user } = useSelector((state) => state.auth);
+  const accessToken = useSelector((state) => state.auth.accessToken);
+
+  const socketUrl =
+    process.env.NEXT_PUBLIC_SOCKET_URL || "http://10.10.20.9:9000";
+
+  useEffect(() => {
+ 
+    if (!accessToken || !user) {
+      if (socketRef.current) {
+        socketRef.current.disconnect();
+        socketRef.current = null;
+      }
+      return;
+    }
+
+    setSocketLoading(true);
+
+    // Initialize socket
+    const socket = io(socketUrl, {
+      auth: { token: accessToken },
+      transports: ["websocket"],
+      withCredentials: true,
+    });
+
+    socketRef.current = socket;
+
+    // Connection success
+    socket.on("connect", () => {
+      setSocketLoading(false);
+      setIsConnected(true);
+      console.log("Socket connected ");
+    });
+
+    // Disconnected
+    socket.on("disconnect", () => {
+      setIsConnected(false);
+      console.log("Socket disconnected ");
+    });
+
+    // Error handler
+    socket.on("socket-error", (error) => {
+      console.error("Socket error:", error);
+    });
+
+    // Cleanup
+    return () => {
+      if (socketRef.current) {
+        socketRef.current.disconnect();
+        socketRef.current = null;
+      }
+    };
+  }, [accessToken, user]);
+
+  return (
+    <SocketContext.Provider
+      value={{
+        socket: socketRef.current,
+        socketLoading,
+        isConnected,
+      }}
+    >
+      {children}
+    </SocketContext.Provider>
+  );
+};
